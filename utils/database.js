46d4d43af1de6466a46884d458fba3f45e58226a
Wyn Chrsitian <80365421+Wyn-Christian@users.noreply.github.com>
2023-10-08 10:50:10 +0800

    Initialize database utils

---
+++ utils/database.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+let isConnected = false;
+export const connectToDB = async () => {
+  mongoose.set("strictQuery", true);
+
+  if (isConnected) {
+    console.log("MongoDB is already connected");
+    return;
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "ad_astra_dev",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.log(error);
+  }
+};
