523a4e071c6eb8746ea33dbd439ae76ee4c5ed3d
Lanceee <lancelurance.sedutan@tup.edu.ph>
2023-10-08 10:06:43 +0800

    Apply page init

---
+++ app/apply/page.js
@@ -0,0 +1,13 @@
+import React from "react";
+import HomeMenuBar from "@/components/landing_page/HomeMenuBar";
+
+function Apply() {
+  return (
+    <div>
+      <HomeMenuBar />
+      Apply
+    </div>
+  );
+}
+
+export default Apply;
